f8401813b34b85fb4871c3610777d8b92297df41
Miguel Botelho <m_botelho8@hotmail.com>
2016-03-21 21:08:34 +0000

    update on Send Message.

--- lan_cloud/src/com/sdis1516t1g02/channels/Channel.java
+++ lan_cloud/src/com/sdis1516t1g02/channels/Channel.java
@@ -2,7 +2,9 @@ package com.sdis1516t1g02.channels;
 
 import com.sdis1516t1g02.Server;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
@@ -13,6 +15,8 @@ import java.util.ArrayList;
  */
 public abstract class Channel implements Runnable {
     protected final static char[] CRLF = {0xD,0xA};
+
+    protected final String file = "files/log.txt";
 
     MulticastSocket mSocket;
     InetAddress multicastAddress;
@@ -26,6 +30,20 @@ public abstract class Channel implements Runnable {
 
         this.multicastAddress = multicastAddress;
         this.mport = mport;
+    }
+
+    protected String getFile() {
+        return file;
+    }
+
+    protected void updateLogger(String header) {
+        try {
+            PrintWriter out = new PrintWriter(file);
+            out.println(header);
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     protected byte[] getHeaderLineFromSegment(byte[] message, int i) throws ChannelException {
@@ -61,7 +79,7 @@ public abstract class Channel implements Runnable {
             }
         }
 
-        //TODO resolver questao de como efectuar quando o header contem várias header lines
+        //TODO resolver questao do body header estar divido em varias linhas
         String header = headerLines.get(0);
         int bodyLength = message.length - i;
         byte[] body = new byte[bodyLength];
@@ -77,6 +95,7 @@ public abstract class Channel implements Runnable {
         byte[] buf = message.getBytes();
         DatagramPacket datagramPacket = new DatagramPacket(buf,buf.length,multicastAddress,mport);
 
+        this.updateLogger(message);
         mSocket.send(datagramPacket);
     }
 }
