a88947da3e6540b29b160577a85b619410152094
444Duarte <duartemr.pinto@gmail.com>
2016-03-22 22:43:39 +0000

    Message Header- Separating header from body improved.

--- lan_cloud/src/com/sdis1516t1g02/channels/Channel.java
+++ lan_cloud/src/com/sdis1516t1g02/channels/Channel.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
  * Created by Duarte on 19/03/2016.
  */
 public abstract class Channel implements Runnable {
-    protected final static char[] CRLF = {0xD,0xA};
+    protected final static String CRLF = "\r\n";
 
     MulticastSocket mSocket;
     InetAddress multicastAddress;
@@ -28,44 +28,15 @@ public abstract class Channel implements Runnable {
         this.mport = mport;
     }
 
-    protected byte[] getHeaderLineFromSegment(byte[] message, int i) throws ChannelException {
-        int init_i = i;
-        for(;i < message.length; i=+2){
-            if(i < message.length-1 && message[i] == CRLF[0] && message[i+1] == CRLF[i+1])  //FIM DA MENSAGEM
-                break;
-        }
-
-        if(i >= message.length)
-            throw new ChannelException("Header Line without end sequence <CRLF>");
-        int lenght = i- init_i;
-        byte[] headerLine = new byte[lenght];
-
-        System.arraycopy(message,init_i,headerLine,0,lenght);
-        return headerLine;
-    }
-
     protected void handleReceivedPacket(DatagramPacket mpacket) throws ChannelException {
         byte[] message = mpacket.getData();
 
-        int i;
-        ArrayList<String> headerLines = new ArrayList<>();
-
-        for ( i = 0; i < message.length; i+=2) {                 //PERCORRE A MENSAGEM DE CHAR EM CHAR
-            if(i < message.length-1 && message[i] == CRLF[0] && message[i+1] == CRLF[i+1])  //FIM DO HEADER
-                break;
-            else{
-                byte[] headerLineBytes = getHeaderLineFromSegment(message, i);      //RETIRA HEADER LINE E AVANÇA PARA O PROXIMO HEADER LINE
-                i += headerLineBytes.length;
-                String headerl = new String(headerLineBytes);
-                headerLines.add(headerl);
-            }
-        }
+        String messageStr = new String(message,0,mpacket.getLength());
+        String splitMessage[] = messageStr.split("\\r\\n");
 
         //TODO resolver questao de como efectuar quando o header contem várias header lines
-        String header = headerLines.get(0);
-        int bodyLength = message.length - i;
-        byte[] body = new byte[bodyLength];
-        System.arraycopy(message,i,body,0,bodyLength);
+        String header = splitMessage[0];
+        byte[] body = splitMessage[splitMessage.length-1].getBytes();
         try {
             handleMessage(header,body);
         } catch (MessageException e) {
