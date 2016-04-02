a17dbc921f42f54ee208418756d5b409b7c98cdd
Miguel Botelho <m_botelho8@hotmail.com>
2016-04-02 21:26:24 +0100

    fds vamos contratar o RevTut para esta merda

--- lan_cloud/src/com/sdis1516t1g02/channels/ChannelException.java
+++ lan_cloud/src/com/sdis1516t1g02/channels/ChannelException.java
@@ -5,14 +5,26 @@ package com.sdis1516t1g02.channels;
  */
 public class ChannelException extends Exception {
 
+    /**
+     * Creates a new Channel Exception, based on a string.
+     * @param message
+     */
     public ChannelException(String message) {
         super(message);
     }
 
+    /**
+     * Creates a new Channel Exception, based on the cause (a Throwable).
+     * @param cause
+     */
     public ChannelException(Throwable cause) {
         super(cause);
     }
 
+    /**
+     * Returns the message of the exception.
+     * @return message
+     */
     @Override
     public String getMessage() {
         return super.getMessage();
